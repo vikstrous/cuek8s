b03a43b7a7c2679257d587ef9b964f9e1cdfaf6f
Viktor Stanchev <me@viktorstanchev.com>
2024-06-24 13:18:21 +0000

    delete gen directory

--- Makefile
+++ Makefile
@@ -1,7 +1,4 @@
 SHELL := /bin/bash
 
-generate:
-	cue get go k8s.io/api/core/v1
-
 publish:
 	@ TAG=$$(git tag --points-at HEAD); if [ "$$TAG" == "" ]; then echo Not tagged; exit 1; fi; cue mod publish "$$TAG"
--- generate.sh
+++ generate.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 # This script is a hack to copy the gen directory into the body of the module because modules can't use a gen directory right now. See https://github.com/cue-lang/cue/issues/3233
 
+cue get go k8s.io/api/core/v1
 rm -rf gen
 cp -r ./cue.mod/gen ./gen
+rm -rf cue.mod/gen
 find ./gen -type f -exec sed -i -E '/import \(/,/^\)/{s/"([^"]*)"/"github.com\/vikstrous\/cuek8s\/gen\/\1"/g}' {} +
